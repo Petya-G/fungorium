bec6cf7ea5285e2a0f68828d157e76300b5bb6bc
Horváth Avarka <avarkahorvath@gmail.com>
2025-03-23 01:02:15 +0100

    commented insect classes

--- 6/skeleton/src/insect/IInsect.java
+++ 6/skeleton/src/insect/IInsect.java
@@ -4,8 +4,28 @@ import mushroom.MushroomThread;
 import mushroom.spore.Spore;
 import tecton.Tecton;
 
+/**
+ * Az IInsect interfész egy rovar viselkedését definiálja.
+ */
 public interface IInsect {
+    /**
+     * Megpróbál elfogyasztani egy gombaspórát.
+     * @param sp Elfogyasztandó spóra.
+     * @return igaz, ha sikerült megenni, egyébként hamis.
+     */
     boolean eat(Spore sp);
+
+        /**
+     * Megpróbál mozogni.
+     * @param targetTecton A tekton, amelyre menni szeretne a rovar.
+     * @return igaz, ha a mozgás sikerült, egyébként hamis
+     */
     boolean move(Tecton t);
+
+         /**
+     * Megpróbál elvágni egy gombafonalat.
+     * @param th Elvágandó gombafonal.
+     * @return igaz, ha sikerült elvágni, egyébként hamis.
+     */
     boolean cut(MushroomThread th);
 }
--- 6/skeleton/src/insect/Insecter.java
+++ 6/skeleton/src/insect/Insecter.java
@@ -6,52 +6,90 @@ import core.Debug;
 import core.Player;
 import tecton.Tecton;
 
+/**
+ * Az Insecter osztály egy játékos, aki egy rovart irányít.
+ * Ez az osztály implementálja az IInsect interfészt, hogy a játékos képes legyen spórákat enni,
+ * mozogni és gombafonalakat elvágni.
+ */
 public class Insecter extends Player implements IInsect {
     private Insect insect;
 
     /**
-     * A rovarászhoz tartozó rovar lekérése
+     * A rovarászhoz tartozó rovart adja vissza.
+     * @return A rovarászhoz tartozó rovar.
      */
     public Insect getInsect() {
+        Debug.DBGFUNC("Rovarászhoz tartozó rovar");
         return insect;
     }
 
     /**
-     * Rovar rendelése a rovarászhoz
+     * Rovart rendel a rovarászhoz.
+     * @param insect A rovar, amelyet a rovarászhoz rendelünk.
      */
     public void setInsect(Insect insect) {
+        Debug.DBGFUNC("Rovarászhoz tartozó rovar beállítása");
         this.insect = insect;
     }
 
+    /**
+     * A kör végén végrehajtandó műveletek.
+     */
     @Override
     public void endTurn() {
-        System.out.println("Spora megevése");
+        Debug.DBGFUNC("Kör vége");
     }
 
+    /**
+     * Visszaadja a rovarász pontszámát.
+     * @return A rovarász pontszáma.
+     */
     @Override
     public Integer getScore() {
+        Debug.DBGFUNC("Rovarász pontszáma");
         return score;
     }
 
+    /**
+     * Növeli a játékos pontszámát.
+     * @param s A hozzáadandó pontok száma.
+     */
     @Override
-    public void addScore(Integer score) {
-        score++;
+    public void addScore(Integer s) {
         Debug.DBGFUNC("Pont megnövelve");
+        score+=s;
     }
 
+    /**
+     * A rovarász megpróbál elfogyasztani egy gombaspórát.
+     * @param sp Az elfogyasztandó spóra.
+     * @return Igaz, ha sikerült megenni a spórát, egyébként hamis.
+     */
     @Override
     public boolean eat(Spore sp) {
+        Debug.DBGFUNC("Spóra megevése");
         return insect.eat(sp);
-        
     }
 
+    /**
+     * A rovarász megpróbál átmenni egy másik tektonra.
+     * @param t A tekton, ahová a rovarász menni szeretne.
+     * @return Igaz, ha a mozgás sikerült, egyébként hamis.
+     */
     @Override
     public boolean move(Tecton t) {
+        Debug.DBGFUNC("Rovarász mozog");
         return insect.move(t);
     }
 
+    /**
+     * A rovarász megpróbál elvágni egy gombafonalat.
+     * @param th Az elvágandó gombafonal.
+     * @return Igaz, ha sikerült elvágni a fonalat, egyébként hamis.
+     */
     @Override
     public boolean cut(MushroomThread th) {
+        Debug.DBGFUNC("Rovarász gombafonalat vág");
         return insect.cut(th);
     }
 }
