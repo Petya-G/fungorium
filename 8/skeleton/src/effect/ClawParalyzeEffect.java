63ba22b9d1276b1fe265f2152d7d9fcee215652c
avarkahorvath <avarkahorvath@gmail.com>
2025-04-10 09:18:49 +0200

    removedd remove from effect classes

--- 8/skeleton/src/effect/ClawParalyzeEffect.java
+++ 8/skeleton/src/effect/ClawParalyzeEffect.java
@@ -20,13 +20,4 @@ public class ClawParalyzeEffect extends Effect {
        i.setClawParalyzed(true);
        Debug.DBGFUNC("Rágó kábító hatás van érvényben a rovaron");
     }
-
-    /**
-     * Érvényteleníti a hatást egy rovarra
-     * @param i A rovar, amire érvényteleníti hatását
-     */
-    public void remove(Insect i) {
-        i.setClawParalyzed(false);
-        Debug.DBGFUNC("Rágó kábító hatás lejárt");
-    }
 }
