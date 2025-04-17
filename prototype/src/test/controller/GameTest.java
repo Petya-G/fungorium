a218cbc2a4e06b36b7dce2d195ff5a11717dc673
Petya-G <vegvaripeter21@gmail.com>
2025-04-17 15:32:05 +0200

    added junit

---
+++ prototype/src/test/controller/GameTest.java
@@ -0,0 +1,64 @@
+package test.controller;
+
+class GameTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCurrentPlayer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWinner() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void move() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void eat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void cut() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void plantMushroomStem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void growThread() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void levelUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void endRound() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void endTurn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+}
