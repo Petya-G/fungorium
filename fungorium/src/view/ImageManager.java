3ca8d04152508c4dd12e8ff0a8361155c1f2b538
avarkahorvath <avarkahorvath@gmail.com>
2025-05-09 23:15:52 +0200

    started image loading

---
+++ fungorium/src/view/ImageManager.java
@@ -0,0 +1,66 @@
+package view;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.*;
+
+public class ImageManager {
+    private static final Map<String, ImageIcon> icons = new HashMap<>();
+    private static final String BASE_PATH = "/icons/";
+
+    static {
+        loadPlayerImages();
+        loadMushroomImages();
+        loadSporeImages();
+        loadTectonImages();
+    }
+
+    private static void loadPlayerImages() {
+        for (int i = 1; i <= 4; i++) {
+            icons.put("insect_normal_" + i, loadIcon(BASE_PATH + "insect/normal/insect_normal_" + i + ".png"));
+            icons.put("insect_paralyzed_" + i, loadIcon(BASE_PATH + "insect/paralyzed/insect_paralyzed_" + i + ".png"));
+        }
+    }
+
+    // Gomba képek betöltése
+    private static void loadMushroomImages() {
+        for (int i = 1; i <= 4; i++) {
+            icons.put("mushroom_stem_" + i, loadIcon(BASE_PATH + "mushroom/stem/mushroom_stem_" + i + ".png"));
+            icons.put("mushroom_thread_" + i, loadIcon(BASE_PATH + "mushroom/thread/mushroom_thread_" + i + ".png"));
+        }
+    }
+
+    // Spóra képek betöltése
+    private static void loadSporeImages() {
+        String[] sporeTypes = {"clawparalyzing", "paralyzing", "slowing", "speeding", "splitting"};
+        for (String type : sporeTypes) {
+            for (int i = 1; i <= 4; i++) {
+                icons.put("spore_" + type + "_" + i, loadIcon(BASE_PATH + "spore/" + type + "/spore_" + type + "_" + i + ".png"));
+            }
+        }
+    }
+
+    // Tecton képek betöltése
+    private static void loadTectonImages() {
+        icons.put("tecton_basic", loadIcon(BASE_PATH + "tecton/tecton_basic.png"));
+        icons.put("tecton_lifesupport", loadIcon(BASE_PATH + "tecton/tecton_lifesupport.png"));
+        icons.put("tecton_singlethreaded", loadIcon(BASE_PATH + "tecton/tecton_singlethreaded.png"));
+        icons.put("tecton_stemless", loadIcon(BASE_PATH + "tecton/tecton_stemless.png"));
+        icons.put("tecton_threadconsuming", loadIcon(BASE_PATH + "tecton/tecton_threadconsuming.png"));
+    }
+
+    // Kép betöltése biztonságosan
+    private static ImageIcon loadIcon(String path) {
+        try {
+            return new ImageIcon(ImageManager.class.getResource(path));
+        } catch (Exception e) {
+            System.err.println("Image not found: " + path);
+            return null;
+        }
+    }
+
+    // Kép lekérdezése dinamikusan
+    public static ImageIcon getIcon(String key) {
+        return icons.get(key);
+    }
+}
